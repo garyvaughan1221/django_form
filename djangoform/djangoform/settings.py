e58b1da8d16338c677af82fd51c89bc2478f7bb3
Gary Leroy Vaughan Jr <5483798+leroyvaughan@users.noreply.github.com>
2025-11-28 13:22:07 -0800

    - added static files   > pulled out css from churches.html >> static/css/churches.css   > added loader_img subtemplate (css is in the template itself)   > added loader_img.png (for fun was playing with static to learn)   > a bunch of stuff that the app included itself for /admin

--- djangoform/djangoform/settings.py
+++ djangoform/djangoform/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for djangoform project.
 
@@ -137,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
