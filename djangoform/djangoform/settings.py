55b9567618400bcaf2c40ba3c3d7d08a8fe47387
Gary Leroy Vaughan Jr <5483798+leroyvaughan@users.noreply.github.com>
2025-11-29 17:19:58 -0800

    adding STATICFILES_DIRS

--- djangoform/djangoform/settings.py
+++ djangoform/djangoform/settings.py
@@ -139,7 +139,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+        # Add other directories if needed
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
