0380229f0e5492a3f45f73950ae69fdba58d408e
Gary Leroy Vaughan Jr <5483798+leroyvaughan@users.noreply.github.com>
2025-12-17 18:47:47 -0800

    more cleanup

--- djangoform/djangoform/views.py
+++ djangoform/djangoform/views.py
@@ -1,53 +1,10 @@
 from datetime import date
 from django.shortcuts import render, redirect
-from .forms import form1 as f
 from .forms import churches as c
 from .forms import state_names as sn
 from .forms import metro_names as mn
 import sys
 from typing import Any, Dict
-
-
-
-def form1_view(request):
-    """View used for html/input_form.html template
-
-    Handles POST and GET methods
-    """
-
-    if request.method == "POST":
-        form = f.InputForm(request.POST)
-
-        if form.is_valid():
-            # Process the form data
-            acceptedNames = ["BlackIce", "SweetTea", "Pythonista", "CodeMaster"]
-            name = form.cleaned_data.get("handle")
-            if name not in acceptedNames:
-                form.add_error("handle", "This handle is invalid. Try again poser!")
-
-            acceptedPasscodes = ["alpha1", "bravo2", "charlie3", "delta4"]
-            passcode = form.cleaned_data.get("passcode")
-            if passcode not in acceptedPasscodes:
-                # Passcode is not accepted
-                form.add_error("passcode", "This passcode is invalid. Try again or prepare for consequences...")
-
-            regDate = form.cleaned_data.get("date")
-            if regDate is not None:
-                if regDate < date(2012, 12, 21):
-                    form.add_error("date", "You are not of the Warrior sect.")
-
-            if form.errors:
-                return render(request, "input_form.html", {"form": form})
-            else:
-                return render(request, "submitted.html", {"form": form})
-    else:
-        # Initial GET request
-        form = f.InputForm()
-    return render(request, "input_form.html", {"form1": form})
-
-
-
-
 
 
 
