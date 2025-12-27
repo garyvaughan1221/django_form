bf6b92d2f7cb15fdcc1d1f43290307b152989d12
Gary Leroy Vaughan Jr <5483798+leroyvaughan@users.noreply.github.com>
2025-12-26 16:57:22 -0800

    fixing 'create table' error on pythonanywhere

--- datasets/db_schemas/mysql_schema.sql
+++ datasets/db_schemas/mysql_schema.sql
@@ -1,15 +1,15 @@
 sql
 
 CREATE TABLE state_names (
-  `StateCode` TEXT NOT NULL,
-  StateName TEXT NOT NULL,
+  `StateCode` VARCHAR(2) NOT NULL,
+  StateName VARCHAR(7) NOT NULL,
   PRIMARY KEY (`StateCode`),
   UNIQUE KEY stateNames_StateCode_key (`StateCode`)
 );
 
 CREATE TABLE county_names (
-  `StateCode` TEXT NOT NULL,
-  CountyName TEXT NOT NULL,
+  `StateCode` VARCHAR(2) NOT NULL,
+  CountyName VARCHAR(14) NOT NULL,
   FIPS INT NOT NULL,
   PRIMARY KEY (FIPS),
   UNIQUE KEY county_names_FIPS_key (FIPS),
@@ -19,8 +19,8 @@ CREATE INDEX IF NOT EXISTS statecode_countynames ON county_names (`StateCode`);
 CREATE INDEX IF NOT EXISTS fips_countynames ON county_names (FIPS);
 
 CREATE TABLE church_orgs (
-  GroupName TEXT NOT NULL,
-  `GroupCode` TEXT NOT NULL,
+  GroupName VARCHAR(20) NOT NULL,
+  `GroupCode` VARCHAR(3) NOT NULL,
   PRIMARY KEY (`GroupCode`),
   UNIQUE KEY church_orgs_GroupCode_key (`GroupCode`),
   UNIQUE KEY church_orgs_GroupName_key (GroupName)
@@ -31,7 +31,7 @@ CREATE INDEX IF NOT EXISTS groupcode_churchorgs ON church_orgs (`GroupCode`);
 
 CREATE TABLE national (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  `GroupCode` VARCHAR(255) NOT NULL,
+  `GroupCode` VARCHAR(3) NOT NULL,
   Congregations INT NULL,
   Adherents INT NULL,
   Adherents_percent_of_Total_Adherents FLOAT NULL,
@@ -40,8 +40,8 @@ CREATE TABLE national (
 
 CREATE TABLE by_state (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  `StateCode` TEXT NOT NULL,
-  `GroupCode` TEXT NOT NULL,
+  `StateCode` VARCHAR(2) NOT NULL,
+  `GroupCode` VARCHAR(3) NOT NULL,
   Congregations INT NULL,
   Adherents INT NULL,
   Adherents_percent_of_Total_Adherents FLOAT NULL,
@@ -53,9 +53,9 @@ CREATE INDEX IF NOT EXISTS statecode_bystate ON by_state (`StateCode`);
 
 CREATE TABLE by_county (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  `StateCode` TEXT NOT NULL,
+  `StateCode` VARCHAR(2) NOT NULL,
   FIPS INT NOT NULL,
-  `GroupCode` TEXT NOT NULL,
+  `GroupCode` VARCHAR(3) NOT NULL,
   Congregations INT NULL,
   Adherents INT NULL,
   Adherents_percent_of_Total_Adherents FLOAT NULL,
