0232524d63b48d12ff107c2a7fe1c8e757fddedf
Gary Leroy Vaughan Jr <5483798+leroyvaughan@users.noreply.github.com>
2025-12-26 16:35:59 -0800

    msyql schema   . first try

---
+++ datasets/db_schemas/mysql_schema.sql
@@ -0,0 +1,69 @@
+sql
+
+CREATE TABLE county_names (
+  `StateCode` TEXT NOT NULL,
+  CountyName TEXT NOT NULL,
+  FIPS INT NOT NULL,
+  PRIMARY KEY (FIPS),
+  UNIQUE KEY county_names_FIPS_key (FIPS),
+  CONSTRAINT county_names_StateCode_fkey FOREIGN KEY (`StateCode`) REFERENCES state_names (`StateCode`)
+);
+CREATE INDEX IF NOT EXISTS statecode_countynames ON county_names (`StateCode`);
+CREATE INDEX IF NOT EXISTS fips_countynames ON county_names (FIPS);
+
+CREATE TABLE state_names (
+  `StateCode` TEXT NOT NULL,
+  StateName TEXT NOT NULL,
+  PRIMARY KEY (`StateCode`),
+  UNIQUE KEY stateNames_StateCode_key (`StateCode`)
+);
+
+CREATE TABLE church_orgs (
+  GroupName TEXT NOT NULL,
+  `GroupCode` TEXT NOT NULL,
+  PRIMARY KEY (`GroupCode`),
+  UNIQUE KEY church_orgs_GroupCode_key (`GroupCode`),
+  UNIQUE KEY church_orgs_GroupName_key (GroupName)
+);
+CREATE INDEX IF NOT EXISTS groupcode_churchorgs ON church_orgs (`GroupCode`);
+
+
+
+CREATE TABLE national (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  `GroupCode` VARCHAR(255) NOT NULL,
+  Congregations INT NULL,
+  Adherents INT NULL,
+  Adherents_percent_of_Total_Adherents FLOAT NULL,
+  Adherents_percent_of_Population FLOAT NULL
+);
+
+CREATE TABLE by_state (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  `StateCode` TEXT NOT NULL,
+  `GroupCode` TEXT NOT NULL,
+  Congregations INT NULL,
+  Adherents INT NULL,
+  Adherents_percent_of_Total_Adherents FLOAT NULL,
+  Adherents_percent_of_Population FLOAT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT by_state_StateCode_fkey FOREIGN KEY (`StateCode`) REFERENCES state_names (`StateCode`)
+);
+CREATE INDEX IF NOT EXISTS statecode_bystate ON by_state (`StateCode`);
+
+CREATE TABLE by_county (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  `StateCode` TEXT NOT NULL,
+  FIPS INT NOT NULL,
+  `GroupCode` TEXT NOT NULL,
+  Congregations INT NULL,
+  Adherents INT NULL,
+  Adherents_percent_of_Total_Adherents FLOAT NULL,
+  Adherents_percent_of_Population FLOAT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT by_county_GroupCode_fkey FOREIGN KEY (`GroupCode`) REFERENCES church_orgs (`GroupCode`),
+  CONSTRAINT by_county_StateCode_fkey FOREIGN KEY (`StateCode`) REFERENCES state_names (`StateCode`)
+);
+CREATE INDEX IF NOT EXISTS fips_bycounty ON by_county (FIPS);
+CREATE INDEX IF NOT EXISTS statecode_bycounty ON by_county (`StateCode`);
+
